838fcec0e2c0d59f7c4875e7e41cbe8fc47ddc94
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-07-13 14:39:19 +0000

    build based on 1760194

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#GAIO.jl","page":"Home","title":"GAIO.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Hello world!","category":"page"}]
+}
