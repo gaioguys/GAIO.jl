f1f14a2803772c658b2027da32bae4048a51debb
mahermann <martin.hermann@tum.de>
2020-08-21 17:01:56 +0200

    add ftle implementation, double gyre example and notebook

--- src/GAIO.jl
+++ src/GAIO.jl
@@ -15,7 +15,10 @@ export dimension, depth
 export BoxMap, PointDiscretizedMap
 export boxmap
 
-export relative_attractor, unstable_set!, chain_recurrent_set, cover_roots
+export BoxFun
+
+export relative_attractor, unstable_set!, chain_recurrent_set, cover_roots,
+       finite_time_lyapunov_exponents
 
 export plot
 
@@ -26,6 +29,7 @@ abstract type BoxPartition{B <: Box} end
 include("partition_regular.jl")
 include("partition_tree.jl")
 include("boxset.jl")
+include("boxfun.jl")  # TODO: borrowed from #33 for now
 include("boxmap.jl")
 include("algorithms.jl")
 
