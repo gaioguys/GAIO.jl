c0dd6adc6f1f9a98baa5e18a5e794b3b94c2c332
gaioguy <oj@tum.de>
2021-05-28 17:13:20 +0200

    plotting in Henon and Lorenz examples

--- examples/henon.jl
+++ examples/henon.jl
@@ -1,20 +1,18 @@
 using GAIO
-using BenchmarkTools
 
-function henon()
-    n = 10
-    grid = LinRange(-1, 1, n)
-    points = collect(Iterators.product(grid, grid))
-    f(x) = (1/2 - 2*1.4*x[1]^2 + x[2], 0.3*x[1])
+n = 10
+grid = LinRange(-1, 1, n)
+points = collect(Iterators.product(grid, grid))
 
-    g = PointDiscretizedMap(f, points)
-    domain = Box((0.0, 0.0), (1.0, 1.0))
-    partition = RegularPartition(domain)
-    boxset = partition[:]
+f(x) = (1/2 - 2*1.4*x[1]^2 + x[2], 0.3*x[1])
+boxmap = PointDiscretizedMap(f, points)
 
-    A = relative_attractor(g, boxset, 26)
-    @btime map_boxes($g,$A)
-    @btime map_boxes_new($g,$A)
-end
+domain = Box((0.0, 0.0), (1.0, 1.0))
+partition = RegularPartition(domain)
+boxset = partition[:]
 
-henon()
+steps = 18
+A = relative_attractor(boxmap, boxset, steps)
+
+plot(A)
+
