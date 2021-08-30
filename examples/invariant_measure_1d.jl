49ad4a21676cf25a26445e2dc8755ee886e5f5f4
Oliver Junge <oj@tum.de>
2021-08-30 16:32:03 +0200

    invariant_measure_1d

--- examples/invariant_measure_1d.jl
+++ examples/invariant_measure_1d.jl
@@ -1,16 +1,16 @@
 using GAIO
-using WGLMakie
-WGLMakie.activate!()
 
-f(x) = 4.0.*x.*(1-x)
-points = 2*rand(1000) .- 1
-F = PointDiscretizedMap(f, points)
+center, radius = 0.5, 0.5
+Q = Box(center, radius)
 
-domain = Box((0.5), (0.5))
-dep = 4
-P = RegularPartition(domain, dep)
-B = P[:]
-Fstar = TransferOperator(F, B)
-(λ, ev) = eigs(Fstar)
+# logistic map
+f(x) = 4.0.*x.*(1.0.-x)
+F = BoxMap(f, Q; no_of_points=400)
+
+depth = 8
+P = RegularPartition(Q, depth)
+
+T = TransferOperator(F, P[:])
+(λ, ev) = eigs(T)
 
 plot(abs ∘ ev[1]; color = :red)
