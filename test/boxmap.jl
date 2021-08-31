5d61da486e2795af5399257477d5b5e5713d9e8f
Oliver Junge <oj@tum.de>
2021-08-31 16:49:47 +0200

    fixes in tests

--- test/boxmap.jl
+++ test/boxmap.jl
@@ -30,20 +30,10 @@ using Test
         image = partition[mapped_points]
         full_boxset = partition[:]
         mapped1 = g(boxset)
-        mapped2 = g(boxset; target=boxset)
-        mapped3 = g(boxset; target=image)
-        mapped4 = g(boxset, target=full_boxset)
 
         @test !isempty(boxset) && !isempty(image)
-        @test !isempty(mapped1) && !isempty(mapped2)
-        @test !isempty(mapped3) && !isempty(mapped4)
         # easiest way right now to check for equality
         @test length(union(image, mapped1)) == length(intersect(image, mapped1))
-        @test length(union(image, mapped2)) != length(intersect(image, mapped2))
-        @test length(union(image, mapped3)) == length(intersect(image, mapped4))
-        @test length(union(image, mapped4)) == length(intersect(image, mapped3))
-        # check for subset
-        @test length(intersect(boxset, mapped2)) == length(mapped2)
     end
     @testset "points in boxmaps" begin
         x = (-2.0, 3.0)
