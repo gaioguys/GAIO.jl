7f29b790c1410da7db994d8cff88ec649f033b49
April-Hannah-Lena <aprillherwig@gmail.com>
2022-01-26 21:40:48 +0100

    rk4 profiling results with FLOPs

--- benchmark/rk4_bench.jl
+++ benchmark/rk4_bench.jl
@@ -1,15 +1,42 @@
 using SIMD, MuladdMacro, LinearAlgebra, StaticArrays, BenchmarkTools
-using LoopVectorization, Random, Plots, DelimitedFiles
-Random.seed!(1234)
+using LoopVectorization, CodeTracking
+using Random, Plots, DelimitedFiles
+#Random.seed!(1234)
 
 const sixth, third = 1/6, 1/3
 const σ, ρ, β = 10.0, 28.0, 0.4
 
+#= function v(du, u)
+    du[1] =     σ * u[2]    -     σ * u[1]
+    du[2] =     ρ * u[1]    -  u[1] * u[3] - u[2]
+    du[3] =  u[1] * u[2]    -     β * u[3]
+    return nothing
+end =#
 function v(u)
     let x=u[1], y=u[2], z=u[3]
         return (σ*(y-x), ρ*x-y-x*z, x*y-β*z)
     end
+end 
+#= function generate_inline(func, du, u, N)
+    str = @code_string func(du, u)
+    str = string((string("  ", line, "\n") for line in split(str, "\n")[2:end-2])...)[1:end-1]
+    str = string(
+        "begin\n", 
+        "function unrolled_f(u_unrolled)\n",
+        "   @assert (n = length(u_unrolled)) % $N == 0\n",
+        "   du_unrolled = similar(u_unrolled)\n",
+        "   @turbo for i in 0:$N:n-$N\n",
+        "       du = @view( du_unrolled[i+1:i+$N] )\n",
+        "       u = u_unrolled[i+1:i+$N]\n",
+        str, 
+        "   end\n",
+        "   return du_unrolled\n",
+        "end\n",
+        "end"
+    )
+    Meta.parse(str)
 end
+ =#
 function unrolled_v(u)
     @assert (nN = length(u)) % 3 == 0
     du = similar(u)
@@ -20,12 +47,25 @@ function unrolled_v(u)
     end
     return du
 end
+function unrolled_v_noturbo(u)
+    @assert (nN = length(u)) % 3 == 0
+    du = similar(u)
+    @muladd @inbounds for i in 0:3:nN-3
+        du[i+1] =     σ * u[i+2]    -     σ * u[i+1]
+        du[i+2] =     ρ * u[i+1]    -  u[i+1] * u[i+3] - u[i+2]
+        du[i+3] =  u[i+1] * u[i+2]  -     β * u[i+3]
+    end
+    return du
+end
 
 benchmarks = Dict()
-timeseries(bench) = map(b -> median(b).time, bench)
+timeseries(bench) = map(b -> 2 * 1e6 / minimum(b).time, bench)
+allocsseries(bench) = map(b -> minimum(b).memory, bench)
 
 u_init(n) = copy(reinterpret(SVector{3,Float64}, 10. .* rand(3*n) .+ fill(10., 3*n)))
 u_16 = u_init(16)
+#@benchmark reshape(permutedims(reinterpret(reshape, Float64, u_init(16))), (3 * 16,))
+#@benchmark vec(PermutedDimsArray(reinterpret(reshape, Float64, u_init(16)), (2,1)))
 
 #= -------------------------------------------- =#
 # serial
@@ -65,11 +105,10 @@ benchmarks["serial"] = [@benchmark $(rk4_flow_map).($v, u) setup=(u=$(u_init)($n
 println("serial finished")
 p = plot(
     8:8:128,
-    timeseries(benchmarks["serial"]),
+    timeseries(benchmarks["serial"]) .* collect(8:8:128),
     lab="serial",
     xlabel="number of points",
-    ylabel="time to eval (ns)",
-    title="Benchmarks of various RK4 Implementations",
+    title="Peak FLOP/s of various RK4 Implementations",
     size=(1200,900),
     leg=:outerbottom
 );
@@ -115,7 +154,7 @@ println("serial, inplace finished")
 p = plot!(
     p,
     8:8:128,
-    timeseries(benchmarks["serial"]),
+    timeseries(benchmarks["serial"]) .* collect(8:8:128),
     lab="serial, inplace"
 );
 
@@ -177,7 +216,7 @@ println("ILP with @turbo finished")
 p = plot!(
     p,
     8:8:128,
-    timeseries(benchmarks["ILP with @turbo"]),
+    timeseries(benchmarks["ILP with @turbo"]) .* collect(8:8:128),
     linestyle=:dash,
     lab="ILP with @turbo"
 );
@@ -242,7 +281,7 @@ println("ILP with @turbo, inplace finished")
 p = plot!(
     p,
     8:8:128,
-    timeseries(benchmarks["ILP with @turbo, inplace"]),
+    timeseries(benchmarks["ILP with @turbo, inplace"]) .* collect(8:8:128),
     linestyle=:dashdot,
     lab="ILP with @turbo, inplace"
 );
@@ -297,7 +336,7 @@ println("ILP with @turbo, unrolled function finished")
 p = plot!(
     p,
     8:8:128,
-    timeseries(benchmarks["ILP with @turbo, unrolled function"]),
+    timeseries(benchmarks["ILP with @turbo, unrolled function"]) .* collect(8:8:128),
     linestyle=:dashdotdot,
     lab="ILP with @turbo, unrolled function"
 );
@@ -355,7 +394,7 @@ println("ILP with @turbo, unrolled function, inplace finished")
 p = plot!(
     p,
     8:8:128,
-    timeseries(benchmarks["ILP with @turbo, unrolled function, inplace"]),
+    timeseries(benchmarks["ILP with @turbo, unrolled function, inplace"]) .* collect(8:8:128),
     linestyle=:dot,
     lab="ILP with @turbo, unrolled function, inplace"
 );
@@ -390,7 +429,8 @@ function rk4_flow_map(f, u::Vector{SVector{N,T}}; τ=0.01, steps=20, simd_length
     m, r = divrem(n, simd_length)
     @assert r == 0
 
-    u_new = reshape(permutedims(reshape(reinterpret(T, u), (N, n))), (N*n,))
+    # u_new = vec(PermutedDimsArray(reinterpret(reshape, T, u), (2,1)))
+    u_new = reshape(permutedims(reinterpret(reshape, T, u)), (N*n,))
     idx = VecRange{simd_length}(1)
     u_tmp = Vector{Vec{simd_length,T}}(undef, N)
     @muladd @inbounds for i in 0:m-1
@@ -413,7 +453,7 @@ println("manually vectorized finished")
 p = plot!(
     p,
     8:8:128,
-    timeseries(benchmarks["manually vectorized"]),
+    timeseries(benchmarks["manually vectorized"]) .* collect(8:8:128),
     marker=:circle,
     lab="manually vectorized"
 );
@@ -448,7 +488,8 @@ function rk4_flow_map!(f, u::Vector{SVector{N,T}}; τ=0.01, steps=20, simd_lengt
     @assert r == 0
     τp2 = τ/2
 
-    u_new = reshape(permutedims(reshape(reinterpret(T, u), (N, n))), (N*n,))
+    #u_new = vec(PermutedDimsArray(reinterpret(reshape, T, u), (2,1)))
+    u_new = reshape(permutedims(reinterpret(reshape, T, u)), (N*n,))
     idx = VecRange{simd_length}(1)
     u_tmp = Vector{Vec{simd_length,T}}(undef, N)
     du, k = similar(u_tmp), similar(u_tmp)
@@ -462,7 +503,7 @@ function rk4_flow_map!(f, u::Vector{SVector{N,T}}; τ=0.01, steps=20, simd_lengt
         end
     end
 
-    u .= copy(reinterpret(SVector{N,T}, reshape(permutedims(reshape(u_new, (n,N))), (N*n,))))
+    u .= reinterpret(SVector{N,T}, reshape(permutedims(reshape(u_new, (n,N))), (N*n,)))
     return nothing
 end
 
@@ -474,7 +515,7 @@ println("manually vectorized, inplace finished")
 p = plot!(
     p,
     8:8:128,
-    timeseries(benchmarks["manually vectorized, inplace"]),
+    timeseries(benchmarks["manually vectorized, inplace"]) .* collect(8:8:128),
     marker=:+,
     lab="manually vectorized, inplace"
 );
@@ -553,7 +594,7 @@ println("manually vectorized, ILP finished")
 p = plot!(
     p,
     8:8:128,
-    timeseries(benchmarks["manually vectorized, ILP"]),
+    timeseries(benchmarks["manually vectorized, ILP"]) .* collect(8:8:128),
     marker=:diamond,
     lab="manually vectorized, ILP"
 );
@@ -622,7 +663,7 @@ end
         u_new[idx + simd_length*i + n*j] = u_tmp[N*i + j + 1]
     end
 
-    u .= copy(reinterpret(SVector{N,T}, reshape(permutedims(reshape(u_new, (n,N))), (N*n,))))
+    u .= reinterpret(SVector{N,T}, reshape(permutedims(reshape(u_new, (n,N))), (N*n,)))
     return nothing
 end
 
@@ -634,23 +675,179 @@ println("manually vectorized, ILP, inplace finished")
 p = plot!(
     p,
     8:8:128,
-    timeseries(benchmarks["manually vectorized, ILP, inplace"]),
+    timeseries(benchmarks["manually vectorized, ILP, inplace"]) .* collect(8:8:128),
     marker=:dtriangle,
     lab="manually vectorized, ILP, inplace"
 );
 
 #= -------------------------------------------- =#
+# manually vectorized, ILP, inplace, unrolled function (no @turbo)
+
+@muladd @inline function rk4!(f, x, dx, k, τ, τp2)
+
+    k .= f(x)
+ @. dx = k * sixth
+ @. k  = x + τp2 * k
+
+    k .= f(k)
+ @. dx = dx + k * third
+ @. k  = x + τp2 * k
+
+    k .= f(k)
+ @. dx = dx + k * third
+ @. k  = x + τ * k
+
+    k .= f(k)
+ @. dx = dx + k * sixth
+ @. x  = x + τ * dx
+
+ return nothing
+end
+
+@inbounds function rk4_flow_map!(f, u::Vector{SVector{N,T}}; τ=0.01, steps=20, simd_length=4) where {N, T}
+    n = length(u)
+    m, r = divrem(n, simd_length)
+    @assert r == 0
+    τp2 = τ/2
+
+    u_new = reshape(permutedims(reshape(reinterpret(T, u), (N, n))), (N*n,))
+    idx = VecRange{simd_length}(1)
+    u_tmp = Vector{Vec{simd_length,T}}(
+        reshape(
+            [u_new[idx + simd_length*i + n*j]
+             for j in 0:N-1, i in 0:m-1],
+            (N*m,)
+        )
+    )
+    du, k = similar(u_tmp), similar(u_tmp)
+
+    for _ in 1:steps
+        rk4!(f, u_tmp, du, k, τ, τp2)
+    end
+    for i in 0:m-1, j in 0:N-1
+        u_new[idx + simd_length*i + n*j] = u_tmp[N*i + j + 1]
+    end
+
+    u .= reinterpret(SVector{N,T}, reshape(permutedims(reshape(u_new, (n,N))), (N*n,)))
+    return nothing
+end
+
+rk4_test = copy(u_16)
+rk4_flow_map!(unrolled_v_noturbo, rk4_test)
+@assert rk4_test ≈ rk4_control
+benchmarks["manually vectorized, ILP, inplace, unrolled function"] = [@benchmark $(rk4_flow_map!)($(unrolled_v_noturbo), u) setup=(u=$(u_init)($n)) for n in 8:8:128]
+println("manually vectorized, ILP, inplace, unrolled function finished")
+p = plot!(
+    p,
+    8:8:128,
+    timeseries(benchmarks["manually vectorized, ILP, inplace, unrolled function"]) .* collect(8:8:128),
+    marker=:star5,
+    lab="manually vectorized, ILP, inplace, unrolled function"
+);
+
+#= -------------------------------------------- =#
+# manually vectorized, ILP, inplace, auto-unrolled function (no @turbo)
+
+#= macro generate_unrolled(f, u, N)
+    quote
+        #println($u)
+        global function Main.$(f)($u; unroll::Val{true})
+            @assert (n = length($u)) % $N == 0
+            du = similar($u)
+            MuladdMacro.@muladd Base.@inbounds for i in 0 : $N : n - $N
+                f(
+                    @view(du[i+1:i+$N]), 
+                    $(u)[i+1:i+$N]
+                )
+            end
+            return du
+        end
+    end
+end
+
+@muladd @inline function rk4!(f, x, dx, k, τ, τp2)
+
+    k .= f(x)
+ @. dx = k * sixth
+ @. k  = x + τp2 * k
+
+    k .= f(k)
+ @. dx = dx + k * third
+ @. k  = x + τp2 * k
+
+    k .= f(k)
+ @. dx = dx + k * third
+ @. k  = x + τ * k
+
+    k .= f(k)
+ @. dx = dx + k * sixth
+ @. x  = x + τ * dx
+
+ return nothing
+end
+
+@inbounds function rk4_flow_map!(f, u::Vector{SVector{N,T}}; τ=0.01, steps=20, simd_length=4) where {N, T}
+    n = length(u)
+    m, r = divrem(n, simd_length)
+    @assert r == 0
+    τp2 = τ/2
+
+    u_new = reshape(permutedims(reshape(reinterpret(T, u), (N, n))), (N*n,))
+    idx = VecRange{simd_length}(1)
+    u_tmp = Vector{Vec{simd_length,T}}(
+        reshape(
+            [u_new[idx + simd_length*i + n*j]
+             for j in 0:N-1, i in 0:m-1],
+            (N*m,)
+        )
+    )
+    du, k = similar(u_tmp), similar(u_tmp)
+
+    for _ in 1:steps
+        rk4!(f, u_tmp, du, k, τ, τp2)
+    end
+    for i in 0:m-1, j in 0:N-1
+        u_new[idx + simd_length*i + n*j] = u_tmp[N*i + j + 1]
+    end
+
+    u .= reinterpret(SVector{N,T}, reshape(permutedims(reshape(u_new, (n,N))), (N*n,)))
+    return nothing
+end
+
+rk4_test = copy(u_16)
+rk4_flow_map!(unrolled_v_noturbo, rk4_test)
+@assert rk4_test ≈ rk4_control
+benchmarks["manually vectorized, ILP, inplace, auto-unrolled function"] = [@benchmark $(rk4_flow_map!)($(unrolled_v_noturbo), u) setup=(u=$(u_init)($n)) for n in 8:8:128]
+println("manually vectorized, ILP, inplace, auto-unrolled function finished")
+p = plot!(
+    p,
+    8:8:128,
+    timeseries(benchmarks["manually vectorized, ILP, inplace, auto-unrolled function"]),
+    marker=:hline,
+    lab="manually vectorized, ILP, inplace, auto-unrolled function"
+); =#
+
+#= -------------------------------------------- =#
 
 key = collect(keys(benchmarks))
-writedlm(
+#= writedlm(
     "./RK4_benchmarks.csv",
     vcat(
         permutedims(key), 
         hcat(
-            [timeseries(benchmarks[k]) for k in key]...
+            [timeseries(benchmarks[k]) .* collect(8:8:128) for k in key]...
         )
     )
 )
+writedlm(
+    "./RK4_allocs_benchmarks.csv",
+    vcat(
+        permutedims(key), 
+        hcat(
+            [allocsseries(benchmarks[k]) for k in key]...
+        )
+    )
+) =#
 
 savefig(p, "./RK4_benchmarks.svg")
 display(p)
