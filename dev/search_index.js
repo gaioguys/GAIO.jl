d254ba60e8db387e1fff11ccf092b2dd9b037398
Documenter.jl <documenter@juliadocs.github.io>
2022-09-14 09:19:43 +0000

    build based on 8f4f949

--- dev/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"#GAIO.jl","page":"Home","title":"GAIO.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Hello world!","category":"page"}]
-}
