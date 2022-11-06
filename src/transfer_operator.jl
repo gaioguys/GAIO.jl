1f526b99b45b59c91931e4646c84742b6c307d40
April-Hannah-Lena <aprillherwig@gmail.com>
2022-11-06 16:11:35 +0100

    COO transferoperator

--- src/transfer_operator.jl
+++ src/transfer_operator.jl
@@ -1,13 +1,19 @@
-mutable struct TransferOperator{B,T,S<:BoxSet{B},M<:BoxMap} <: AbstractSparseMatrix{T,Int}
+struct TransferOperator{B,T,I,S<:BoxSet{B},M<:BoxMap,D<:AbstractDict{Tuple{I,I},T}} <: AbstractMatrix{T}
     F::M
     support::S
-    # it is more convenient to store the transposed matrix
-    mat::SparseMatrixCSC{T,Int}
+    mat::D
 end
 
-# ensure that `TransferOperator` uses an `OrderedSet`
-function TransferOperator(g::BoxMap, boxset::BoxSet{B,Q,S}) where {B,Q,S}
-    TransferOperator(g, BoxSet(boxset.partition, OrderedSet(boxset.set)))
+function TransferOperator(
+        g::M, support::S, mat::D
+    ) where {B,T,I,Y<:OrderedSet,S<:BoxSet{B,Q,Y},M<:BoxMap,D<:AbstractDict{Tuple{I,I},T}}
+    TransferOperator{B,T,I,S,M,D}(g, support, mat)
+end
+
+function TransferOperator(
+        g::M, support::S, mat::D
+    ) where {B,T,I,Y,S<:BoxSet{B,Q,Y},M<:BoxMap,D<:AbstractDict{Tuple{I,I},T}}
+    TransferOperator{B,T,I,S,M,D}(g, BoxSet(support.partition, OrderedSet(support.set)), mat)
 end
 
 # helper function so we aren't doing type piracy on `mergewith!`
@@ -21,12 +27,10 @@ end
 
 function TransferOperator(
         g::BoxMap, boxset::BoxSet{B,Q,S}
-    ) where {N,T,B<:Box{N,T},Q<:BoxPartition,S<:OrderedSet}
+    ) where {N,T,B<:Box{N,T},Q<:BoxPartition,S}
 
-    OrderedCollections.rehash!(boxset.set.dict)
     P, D = boxset.partition, Dict{Tuple{keytype(Q),keytype(Q)},T}
     @floop for key in boxset.set
-        i = getkeyindex(boxset, key)
         box = key_to_box(P, key)
         c, r = box.center, box.radius
         domain_points = g.domain_points(c, r)
@@ -38,34 +42,39 @@ function TransferOperator(
             r = hitbox.radius
             for ip in g.image_points(c, r)
                 hit = point_to_key(P, ip)
-                j = getkeyindex(boxset, hit)
-                isnothing(j) && continue
-                @reduce( dict = D() ⊔ ((i,j) => inv_n) )
+                isnothing(hit) && continue
+                @reduce( mat = D() ⊔ ((hit,key) => inv_n) )
             end
         end
     end
-    return TransferOperator(g, boxset, sparse(dict, length(boxset), length(boxset)))
+    return TransferOperator(g, boxset, mat)
 end
 
 function Base.show(io::IO, g::TransferOperator)
-    print(io, "TransferOperator with $(length(nonzeros(g.mat))) stored entries over $(g.support.partition)")
+    print(io, "TransferOperator with $(length(g.mat)) stored entries over $(g.partition)")
 end
 
 Base.show(io::IO, ::MIME"text/plain", g::TransferOperator) = show(io, g)
 Base.:(==)(g1::TransferOperator, g2::TransferOperator) = g1.mat == g2.mat
-Base.axes(g::TransferOperator) = (v = collect(g.support); (v, v))
-Base.size(g::TransferOperator) = (length(g.support), length(g.support))
-Base.Matrix(g::TransferOperator) = copy(g.mat')
+Base.size(g::TransferOperator) = ((is, js) = axes(g); (length(is), length(js)))
 Base.eltype(::Type{<:TransferOperator{B,T}}) where {B,T} = T
-Base.keytype(::Type{<:TransferOperator{B,T,I}}) where {B,T,I} = I
+Base.keytype(::Type{<:TransferOperator{B,T,I}}) where {B,T,I} = Tuple{I,I}
 
-function Base.checkbounds(::Type{Bool}, g::TransferOperator, keys)
-    all(x -> checkbounds(Bool, g.support.partition, x), keys) || return false
-    diff = setdiff(keys, g.support.set)
+function Base.axes(g::TransferOperator{B,T,I}) where {B,T,I}
+    is, js = Set{I}(), Set{I}()
+    for ((i, j), w) in g.mat
+        is = is ⊔ i
+        js = js ⊔ j
+    end
+    return collect(is), collect(js)
+end
+
+function Base.checkbounds(::Type{Bool}, g::TransferOperator{B,T,I}, keys) where {B,T,I}
+    all(x -> checkbounds(Bool, g.partition, x), keys) || return false
+    diff = setdiff(keys, axes(g)[2])
     if !isempty(diff)
-        union!(g.support.set, keys)
-        g̃ = TransferOperator(g.F, g.support)
-        g.mat = g̃.mat
+        g̃ = TransferOperator(g.F, BoxSet(g.partition, Set{I}(diff)))
+        g.mat = g.mat ⊔ g̃.mat
     end
     return true
 end
@@ -74,8 +83,7 @@ Base.checkbounds(b::Type{Bool}, g::TransferOperator, key1, keys...) = checkbound
 
 function Base.getindex(g::TransferOperator{T,I}, u, v) where {T,I}
     checkbounds(Bool, g, u, v) || throw(BoundsError(g, (u,v)))
-    i, j = getkeyindex(g.support, u), getkeyindex(g.support, v)
-    return g.mat[i, j]
+    return g.mat[u, v]
 end
 
 function Base.setindex!(g::TransferOperator, u...)
@@ -83,55 +91,62 @@ function Base.setindex!(g::TransferOperator, u...)
 end
 
 for (type, (gmap, ind1, ind2, func)) in Dict(
-        TransferOperator                                    => (:(g),        :j, :i, identity),
-        LinearAlgebra.Transpose{<:Any,<:TransferOperator}   => (:(g.parent), :i, :j, transpose),
-        LinearAlgebra.Adjoint{<:Any,<:TransferOperator}     => (:(g.parent), :i, :j, adjoint)
+        TransferOperator                                    => (:(g),        :i, :j, identity),
+        LinearAlgebra.Transpose{<:Any,<:TransferOperator}   => (:(g.parent), :j, :i, transpose),
+        LinearAlgebra.Adjoint{<:Any,<:TransferOperator}     => (:(g.parent), :j, :i, adjoint)
     )
 
     @eval begin
 
-        LinearAlgebra.issymmetric(g::$type) = issymmetric($gmap.mat)
+        function LinearAlgebra.issymmetric(g::$type)
+            for ((i, j), w) in $gmap.mat
+                w̃ = get($gmap.mat, (j,i), 0)
+                w == w̃ || return false
+            end
+            return true
+        end
 
         function eigenfunctions(g::$type, B=I; nev=1, ritzvec=true, droptol=sqrt(eps(eltype($gmap))), kwargs...)
             λ, ϕ, nconv = Arpack._eigs(g, B; nev=nev, ritzvec=true, kwargs...)
-            b = [BoxFun($gmap.support, ϕ[:, i]) for i in 1:nev]
+            D = OrderedDict{keytype($gmap.support.partition),eltype(ϕ)}
+            b = [
+                BoxFun(
+                    $gmap.partition, 
+                    D(key => val for (key,val) in zip(g.support.set, ϕ[:, i]))
+                ) for i in 1:nev
+            ]
             return ritzvec ? (λ, b, nconv) : (λ, nconv)
         end
 
         Arpack.eigs(g::$type, B::UniformScaling=I; kwargs...) = eigenfunctions(g, B; kwargs...)
         Arpack.eigs(g::$type, B; kwargs...) = eigenfunctions(g, B; kwargs...)
     
-        LinearAlgebra.mul!(y::AbstractVector, g::$type, x::AbstractVector) = mul!(y, func($gmap.mat), x)
-        
-        function LinearAlgebra.mul!(y::BoxFun, g::$type, x::BoxFun)
-            checkbounds(Bool, g, x.support.set) || throw(BoundsError(g, x.support.set))
-            union!(y.support.set, g.support.set)
-            resize!(y.vals, length(y.support.set))
-            mul!(y.vals, g, x.vals)
+        @muladd function LinearAlgebra.mul!(y::AbstractVector, g::$type, x::AbstractVector)
+            s = $gmap.support
+            for ((u, v), w) in $gmap.mat
+                (u in s.set && v in s.set) || continue
+                $ind1, $ind2 = getkeyindex(s, u), getkeyindex(s, v)
+                y[j] = y[j] + w * $func(x[i])
+            end
             return y
         end
 
-        function Base.:(*)(g::$type, x::BoxFun)
-            y = BoxFun(x.support, similar(x.vals))
+        @muladd function LinearAlgebra.mul!(y::BoxFun, g::$type, x::BoxFun)
+            s = $gmap.support
+            for ((u, v), w) in $gmap.mat
+                $ind1, $ind2 = u, v
+                y[j] = y[j] + w * $func(x[i])
+            end
+            return y
+        end
+
+        function Base.:(*)(g::$type, x::BoxFun{B,K,V,P,D}) where {B,K,V,P,D}
+            checkbounds(Bool, $gmap, keys(x.vals)) || throw(BoundsError(g, x))
+            vals = D()
+            sizehint!(vals, length(x))
+            y = BoxFun(x.partition, vals)
             return mul!(y.vals, g, x.vals)
         end
 
     end
-end
-
-# convert DOK sparse matrix to CSC
-function SparseArrays.sparse(dict::Dict{Tuple{I,I},T}, m::Number, n::Number) where {I,T}
-    xs, ys, ws = I[], I[], T[]
-
-    sizehint!(xs, length(dict))
-    sizehint!(ys, length(dict))
-    sizehint!(ws, length(dict))
-
-    for ((x, y), w) in dict
-        push!(xs, x)
-        push!(ys, y)
-        push!(ws, w)
-    end
-
-    return sparse(xs, ys, ws, m, n)
 end
