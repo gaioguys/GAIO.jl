802cc167588130ced8772b9259645a7ae8e82ffb
April Herwig <aprillherwig@gmail.com>
2024-01-08 08:00:09 +0100

    benchmark

---
+++ benchmark/dynamicalsystems_benchmark.jl
@@ -0,0 +1,68 @@
+using GAIO
+using StaticArrays
+using DynamicalSystems, OrdinaryDiffEq, BenchmarkTools
+
+ENV["JULIA_DEBUG"] = "all"
+
+# the Lorenz system
+σ, ρ, β = 10.0, 28.0, 0.4
+p0 = SA_F64[σ,ρ,β]
+
+τ, n_steps = 0.01, 20
+const Δt = τ*n_steps
+
+function lorenz_dudx(u::Vec, p, t, Δt) where Vec
+    x,y,z = u
+    σ,ρ,β = p
+
+    dudx = Vec(( σ*(y-x), ρ*x-y-x*z, x*y-β*z ))
+    return Δt * dudx
+end
+
+lorenz(u, p=p0, t=0) = lorenz_dudx(u, p, t, Δt)
+
+# hand-coded integrator: most efficient (if possible)
+f(x) = rk4_flow_map(lorenz, x, 1/20, 20)
+
+# DynamicalSystem: less efficient but can use DifferentialEquations under the hood
+u0 = rand(SVector{3,Float64})
+
+alg = RK4(thread = OrdinaryDiffEq.False())
+diffeq = (alg = alg, dt = 1/20, adaptive = false)
+dyn_syst = ContinuousDynamicalSystem(lorenz, u0, p0; diffeq)
+
+# DifferentialEquations integrator: least efficient
+prob = ODEProblem(lorenz, u0, (0.,1.), p0)
+
+function h(x)
+    new_prob = remake(prob, u0=x)
+    solution = solve(
+        new_prob, alg, dt = 1/20, 
+        adaptive = false, save_everystep=false, save_start=false
+    )
+    return solution.u[end]
+end
+
+# analysis
+center, radius = (0,0,25), (30,30,30)
+domain = Box(center, radius)
+P = BoxPartition(domain, (128,128,128))
+
+x = ( sqrt(β*(ρ-1)), sqrt(β*(ρ-1)), ρ-1 )   # equilibrium
+S = cover(P, x)
+
+F = BoxMap(:adaptive, f, domain)
+W = unstable_set(F, S)
+
+F = BoxMap(:grid, f, domain, n_points=(2,2,2))
+G = BoxMap(:grid, dyn_syst, domain, n_points=(2,2,2))
+H = BoxMap(:grid, h, domain, n_points=(2,2,2))
+
+@benchmark F(W)
+@benchmark G(W)
+@benchmark H(W)
+
+#using Plots: plot
+using GLMakie
+
+plot(W)
--- ext/DynamicalSystemsExt.jl
+++ ext/DynamicalSystemsExt.jl
@@ -3,33 +3,49 @@ module DynamicalSystemsExt
 using GAIO, DynamicalSystemsBase, Base.Threads
 
 import GAIO: preprocess
-import Base: getindex
+import Base: getindex, iterate
+
+struct LockedSystem{F}
+    lock::ReentrantLock
+    system::F
+end
+
+LockedSystem(system) = LockedSystem(ReentrantLock(), system)
+convert(::Type{T}, system::S) where {T<:LockedSystem,S<:DynamicalSystem} = LockedSystem(system)
+
+Base.iterate(ls::LockedSystem, i...) = (ls.lock, Val(:syst))
+Base.iterate(ls::LockedSystem, ::Val{:syst}) = (ls.system, Val(:done))
+Base.iterate(ls::LockedSystem, ::Val{:done}) = nothing
 
 struct ParallelSystems{N,F}
-    systems::NTuple{N,F}
+    systems::NTuple{N,LockedSystem{F}}
 end
 
 function ParallelSystems(system::DynamicalSystem)
     # we are forced to use the interactive threads since FLoops doesn't 
     # know how to distinguish interactive threads from normal ones
     N = nthreads(:default) + nthreads(:interactive)
-    systems = ntuple(_-> recursivecopy(system), N)
+    systems = ntuple(_-> LockedSystem(recursivecopy(system)), N)
     return ParallelSystems(systems)
 end
 
 function (par::ParallelSystems{N,F})(x) where {N,F<:DiscreteTimeDynamicalSystem}
-    system = par[threadid()]
-    reinit!(system, x)
-    step!(system)
-    @debug "state" system maxlog=16
+    lk, system = par[threadid()]
+    lock(lk) do 
+        reinit!(system, x)
+        step!(system)
+        @debug "state" system maxlog=16
+    end
     return current_state(system)
 end
 
 function (par::ParallelSystems{N,F})(x) where {N,F<:ContinuousTimeDynamicalSystem}
-    system = par[threadid()]
-    reinit!(system, x)
-    step!(system, 1, true)
-    @debug "state" system maxlog=16
+    lk, system = par[threadid()]
+    lock(lk) do
+        reinit!(system, x)
+        step!(system, 1, true)
+        @debug "state" system maxlog=16
+    end
     return current_state(system)
 end
 
